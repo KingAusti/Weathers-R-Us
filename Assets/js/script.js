2e7b5a84d10f6fb40767cdc0649f2e9889349dde
KingAusti <austin@thehenrys.xyz>
2022-04-06 20:34:36 -0700

    adding most fetch commands

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,10 +9,89 @@ let forecastContainerEl = document.querySelector('#fiveday');
 let pastSearchButtonEl = document.querySelector('#past-search');
 let weatherContainerEl = document.querySelector('#current-weather');
 
-let getWeather = function() {    
-    let apiFetch = "api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={0663fc572d76208f0ecd3412b806a465}"
+const API_KEY = "0663fc572d76208f0ecd3412b806a465"
+let LAT
+let LON
+// let TEMP
+// let HUM
+// let WIND
+// let ICON
+// let DESC
+let fiveDay = []
+
+const showToday = () {
+    document.appendChild
+    {temp, desc} = sixDay[0]
+}
+
+const showNextFive = () {
+    for (let i=1; i<sixDay.length; i++) {
+        document.createElement
+    }
+}
+
+const submitForm = function(event) {
+    event.preventDefault();
+    getThatLatMyLon(cityInputEl.value);
+}
+
+const getThatLatMyLon = function(city){
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`)
+    .then(
+       response => response.json()       
+    )
+    .then(
+        data => {
+            LAT = data[0].lat
+            LON = data[0].lon
+            getWeather()
+        }
+    )
+    .catch(err => console.error(err));
+}
+
+// const getWeather = function() {    
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${API_KEY}`)
+//     .then(
+//         response => response.json()
+//     )
+//     .then(
+//         data => {
+//             TEMP = data.main.temp
+//             HUM = data.main.humidity
+//             WIND = data.wind
+//             ICON = data.weather[0].icon
+//             DESC = data.weather[0].description
+//             giveMeFive()     
+//         }
+//     )
+//     .catch(err => console.error(err));
+// }
+
+const giveMeFive = function() {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${LAT}&lon=${LON}&cnt=6&appid=${API_KEY}&units=imperial`)
+    .then(
+        response => response.json()
+    )
+    .then(
+        data => {
+            data.list.forEach(day => {
+                fiveDay.push(
+                    {
+                        temp: day.main.temp,
+                        hum: day.main.humidity,
+                        wind: data.wind.speed,
+                        icon: data.weather[0].icon,
+                        desc: data.weather[0].description
+                    }
+                )
+            })
+            console.log(fiveDay)
+        }
+    )
+    .catch(err => console.error(err))
 }
 
 //event listeners
 cityFormEl.addEventListener('submit', submitForm);
-pastSearchButtonEl.addEventListener('click', pastSearch);
+//pastSearchButtonEl.addEventListener('click', pastSearch);
